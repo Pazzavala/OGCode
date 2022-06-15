4f66199cae02b669d7adf958766caa736f1ee19e
Paz <pazrodriguez@gmail.com>
2022-06-15 00:49:33 -0500

    Adding DoublyLinkedList.java file

---
+++ Java Code/DoublyLinkedList.java
@@ -0,0 +1,12 @@
+// DoublyLinkedList.java
+
+public class DoublyLinkedList <T> implements Iterable <T> {
+	private int size = 0;
+	private Node <T> head = null;
+	private Node <T> Tail = null;
+
+	// Internal Class: Node to represent data
+	private class Node <T> {
+		
+	}
+}
